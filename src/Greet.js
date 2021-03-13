111ff756aa7ca85e349dd6fffb4a0b8490592627
palak <palakchugh2810@gmail.com>
2021-03-13 11:36:12 +0530

    firstCommit
    firstCommit

---
+++ src/Greet.js
@@ -0,0 +1,5 @@
+import React from 'react'
+function Greet()
+{
+    <h1>Hello plucksiiii</h1>
+}
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+ReactDOM.render(<div><h1>hello world.......</h1><p>this is paragraph</p></div>,document.getElementById("root"));
